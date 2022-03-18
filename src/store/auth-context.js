d6d7a39eb8c94decff01c2854523af4cc6d1a2d1
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-03-18 21:32:43 +0100

    Building custom context provider component

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+//! Create context :
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+//? Adding the properties inside the context is just for a better IDE auto-complete.
 
 export default AuthContext;
+
+//! Create custom context provider component :
+
+export const AuthContextProvider = function (props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedLoggedInStatus = localStorage.getItem("isLoggedIn");
+
+    //* Keeping the user logged in although the refresh :
+    if (storedLoggedInStatus === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    //* Store a truty value when the user is logged in :
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    //* Clear the local storage to log out :
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
