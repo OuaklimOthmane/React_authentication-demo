cde31892258771657e6cecfa7b8d4f19aa52fe88
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-03-18 19:52:11 +0100

    Manage the logout function state globaly using use context

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
       <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
